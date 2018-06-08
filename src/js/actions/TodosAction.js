fd4bbcc928c3cd52ba608ff244dae9ecec5401be
CodHeK <gaganganapathyas@yahoo.in>
2018-06-09 01:10:19 +0530

    minor edits

--- src/js/actions/TodosAction.js
+++ src/js/actions/TodosAction.js
@@ -1,9 +1,14 @@
 import md5 from 'md5';
 
 export function AddTodoAction(TodoData) {
+  let d = Date(Date.now()).toString().split(" ");
+  var newDate = "";
+  for(var i=0;i<d.length-2;i++) {
+    newDate += (d[i] + " ");
+  }
   return {
     type: "ADD_NEW_TODO",
-    payload: { id: md5(TodoData) , date: Date(Date.now()).toString(), data: TodoData }
+    payload: { id: md5(TodoData) , date: newDate, data: TodoData }
   }
 }
 
